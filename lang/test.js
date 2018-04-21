492ef1f2cfa7bf7f39024ffcf1d94d858320bdd7
anton feldmann <anton.feldmann@gmail.com>
2018-04-21 18:26:53 +0900

    inti

---
+++ lang/test.js
@@ -0,0 +1,11 @@
+var json = {
+  "en":{
+    "test1":"this is a test",
+    "test2":"this ist test 2"
+  },
+  "de":{
+    "test1":"Dies ist ein Test",
+    "test2":"Dies ist Test 2"
+  },
+  "default":"en"
+};
